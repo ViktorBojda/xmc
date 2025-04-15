eac4e46d27b240c045319df95b3e6e30594c5cab
Viktor Bojda <viktorbojda17@gmail.com>
2025-04-15 20:47:04 +0200

    triv: remove redundant xgb explain_anchors method

--- src/xmc/explainers/xgb.py
+++ src/xmc/explainers/xgb.py
@@ -12,22 +12,3 @@ class MalwareExplainerXGB(TreeMalwareExplainer):
         self, model: Any, feature_names: list[str]
     ) -> shap.TreeExplainer:
         return shap.TreeExplainer(model, feature_names=feature_names)
-
-    @timer
-    def explain_anchors(self) -> None:
-        artifacts = self.classifier_class.load_model_artifacts()
-        model: XGBClassifier = artifacts["model"]
-        feature_names = artifacts["feature_names"]
-        label_encoder = artifacts["label_encoder"]
-        X_train, y_train = artifacts["X_train"], artifacts["y_train"]
-        X_test, y_test = artifacts["X_test"], artifacts["y_test"]
-
-        self.create_anchors_explanations(
-            model.predict,
-            X_train,
-            y_train,
-            X_test,
-            y_test,
-            feature_names,
-            label_encoder,
-        )
