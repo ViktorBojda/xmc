b63f3946ea3527e829be5fd4501a54c38815561a
Viktor Bojda <viktorbojda17@gmail.com>
2025-04-16 13:29:14 +0200

    feat: add alibi compatible tensorflow and set it to use legacy keras

--- src/xmc/settings.py
+++ src/xmc/settings.py
@@ -1,5 +1,10 @@
+import logging
 import os
 from pathlib import Path
+
+os.environ["TF_USE_LEGACY_KERAS"] = "1"  # alibi requires this
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+import tensorflow as tf
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 DATASETS_DIR = os.path.join(ROOT_DIR, "datasets")
@@ -10,3 +15,6 @@ PLOTS_DIR = os.path.join(ROOT_DIR, "plots")
 PLOTS_DIR_PATH = Path(PLOTS_DIR)
 EXPLANATIONS_DIR = os.path.join(ROOT_DIR, "explainers/explanations")
 EXPLANATIONS_DIR_PATH = Path(EXPLANATIONS_DIR)
+
+tf.get_logger().setLevel(logging.ERROR)  # suppress deprecation messages
+tf.compat.v1.disable_v2_behavior()  # disable TF2 behaviour as alibi code still relies on TF1 constructs
